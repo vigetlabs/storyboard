ea310ef974e040464d6e28eb09f6f4d5ed2caeff
Nate Hunzaker <nate.hunzaker@gmail.com>
2018-11-08 21:20:32 -0800

    Add basic story example

---
+++ app/javascript/components/editor/index.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react'
+
+import {
+  DiagramEngine,
+  DiagramModel,
+  DefaultNodeModel,
+  LinkModel,
+  DiagramWidget,
+  DefaultLinkModel
+} from 'storm-react-diagrams'
+
+import 'storm-react-diagrams/dist/style.min.css'
+
+import './editor.css'
+
+const nodes = {
+  hungry: new DefaultNodeModel(
+    'You are hungry. Sooo hungry.',
+    'rgb(0,192,255)'
+  ),
+  cooking: new DefaultNodeModel(
+    'You frying bacon. It smells amazing.',
+    'rgb(0,192,255)'
+  ),
+  starve: new DefaultNodeModel('You ded', 'rgb(0,192,255)')
+}
+
+const ports = {
+  findFood: nodes.hungry.addOutPort('Find Food'),
+  wait: nodes.hungry.addOutPort('Wait'),
+  cook: nodes.cooking.addInPort('Cook'),
+  dead: nodes.starve.addInPort('Death')
+}
+
+function link(a, b, label) {
+  let edge = a.link(b)
+
+  edge.addLabel(label)
+
+  return edge
+}
+
+const Editor = () => {
+  let engine = new DiagramEngine()
+  engine.installDefaultFactories()
+
+  let model = new DiagramModel()
+
+  nodes.hungry.setPosition(100, 100)
+  nodes.cooking.setPosition(400, 300)
+  nodes.starve.setPosition(100, 300)
+
+  model.addAll(
+    nodes.hungry,
+    nodes.cooking,
+    nodes.starve,
+    link(ports.findFood, ports.cook, 'Make bacon'),
+    link(ports.wait, ports.dead, 'Your hearts deplete')
+  )
+
+  engine.setDiagramModel(model)
+
+  return <DiagramWidget className="srd-demo-canvas" diagramEngine={engine} />
+}
+
+export default Editor
--- app/javascript/packs/hello_react.tsx
+++ app/javascript/packs/hello_react.tsx
@@ -1,21 +1,7 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
-// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
-// of the page.
-
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import Editor from '../components/editor'
 
-interface Props {
-  name: String
-}
+import '../components/global.css'
 
-const Hello: React.SFC<Props> = props => <div>Hello {props.name}!</div>
-
-Hello.defaultProps = {
-  name: 'David'
-}
-
-ReactDOM.render(
-  <Hello name="React" />,
-  document.body.appendChild(document.createElement('div'))
-)
+ReactDOM.render(<Editor />, document.getElementById('editor'))
