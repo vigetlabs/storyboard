7264d86db56cbbd0755e1cc38aeb320a5ffa979a
Nate Hunzaker <nate.hunzaker@gmail.com>
2018-11-08 20:00:19 -0800

    Check-in a handeful of configuration files

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -6,9 +6,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+const Hello = props => <div>Hello {props.name}!</div>
 
 Hello.defaultProps = {
   name: 'David'
@@ -21,6 +19,6 @@ Hello.propTypes = {
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
+    document.body.appendChild(document.createElement('div'))
   )
 })
