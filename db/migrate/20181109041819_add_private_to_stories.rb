3ada6055b382a9e637beebeb03678f57b330c8a7
Eli Fatsi <eli.fatsi@viget.com>
2018-11-09 05:43:42 -0700

    Revert erroneous find-and-replace in migrations

--- db/migrate/20181109041819_add_private_to_stories.rb
+++ db/migrate/20181109041819_add_private_to_stories.rb
@@ -1,5 +1,5 @@
 class AddPrivateToStories < ActiveRecord::Migration[5.2]
   def change
-    add_column :adventures, :private, :boolean, default: false
+    add_column :stories, :private, :boolean, default: false
   end
 end
