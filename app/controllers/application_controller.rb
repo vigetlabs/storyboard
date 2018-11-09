e62d25de8d1a2f7080883e09a59ad4c706b67288
Eli Fatsi <eli.fatsi@viget.com>
2018-11-09 06:08:02 -0700

    Redirect to /mine on most logins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_in_path_for(resource)
+    # cache since successive method calls are nil
+    stored = stored_location_for(resource)
+
+    if stored != root_path
+      stored
+    else
+      my_adventures_path
+    end
+  end
+
   def storable_location?
     request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
   end
