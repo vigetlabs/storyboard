58dfa9b240f407b41567437eaaeafed60d4a5e79
Nate Hunzaker <nate.hunzaker@gmail.com>
2018-11-08 20:06:50 -0800

    Add typescript

---
+++ app/javascript/packs/hello_react.tsx
@@ -0,0 +1,21 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+interface Props {
+  name: String
+}
+
+const Hello: React.SFC<Props> = props => <div>Hello {props.name}!</div>
+
+Hello.defaultProps = {
+  name: 'David'
+}
+
+ReactDOM.render(
+  <Hello name="React" />,
+  document.body.appendChild(document.createElement('div'))
+)
