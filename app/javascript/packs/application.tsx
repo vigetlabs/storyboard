d7b5a2aae9e8082c88a3fb62ea7aaea7855a7a2c
Nate Hunzaker <nate.hunzaker@gmail.com>
2018-11-10 10:39:09 -0800

    Start styling the things

--- app/javascript/packs/application.tsx
+++ app/javascript/packs/application.tsx
@@ -1,11 +1,12 @@
 import '../src/global.css'
+import '../src/type.css'
 import '../src/css/default/AccountForm.css'
 import '../src/css/default/AdventureList.css'
 import '../src/css/default/StoryIndex.css'
 import '../src/css/default/button.css'
-import '../src/css/default/global.css'
+import '../src/css/default/ThemeDark.css'
 
 // Forward and back navigation don't cause a reload.  Since we are pulling
 // data from a variable rendered by the server if we don't reload we don't
 // get up to date data.  This is a workaround for that issue.
-if (performance.navigation.type === 2) location.reload(true);
+if (performance.navigation.type === 2) location.reload(true)
