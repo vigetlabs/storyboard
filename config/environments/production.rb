756bd050fac82b9524958a0d55826e7887686556
Eli Fatsi <eli.fatsi@viget.com>
2018-11-13 09:24:01 -0700

    Set action mailer host for prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,4 +98,6 @@ Rails.application.configure do
     authentication:       :plain,
     enable_starttls_auto: true
   }
+
+  config.action_mailer.default_url_options = { host: 'storyboard.viget.com' }
 end
