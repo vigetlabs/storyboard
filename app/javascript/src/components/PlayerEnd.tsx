4ff94087f279a2416cfd88c5dc7760382880e346
Nate Hunzaker <nate.hunzaker@gmail.com>
2019-01-11 08:13:29 -0800

    Fixup markup, improve player component reuse
    Some of our scene markup was a little too fun. This commit fixes that,
    and creates some more reuse across components.

--- app/javascript/src/components/PlayerEnd.tsx
+++ app/javascript/src/components/PlayerEnd.tsx
@@ -10,7 +10,7 @@ export const PlayerEnd: React.SFC<Props> = ({ title, body, onReplay }) => {
   return (
     <main className="PlayerEnd">
       <div className="PlayerForeground">
-        <h1 className="PlayerEndTitle">{ title }</h1>
+        <h1 className="PlayerEndTitle">{title}</h1>
 
         <div className="PlayerEndContent">
           <div
@@ -24,5 +24,19 @@ export const PlayerEnd: React.SFC<Props> = ({ title, body, onReplay }) => {
         </div>
       </div>
     </main>
+  )
+}
+
+interface DeadEndProps {
+  onReplay: () => void
+}
+
+export const PlayerDeadEnd: React.SFC<DeadEndProps> = ({ onReplay }) => {
+  return (
+    <PlayerEnd
+      title="Dead End"
+      body="That choice wasn't tied to a new scene. You should fix that. Try replaying the story."
+      onReplay={onReplay}
+    />
   )
 }
