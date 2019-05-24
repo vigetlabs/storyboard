102813b72050bc8b0b9ffe4ca06313defe9877e8
Eli Fatsi <eli.fatsi@viget.com>
2019-05-24 13:47:21 -0600

    Add button to open tutorial in bottom right

--- app/javascript/src/components/Tutorial.tsx
+++ app/javascript/src/components/Tutorial.tsx
@@ -14,39 +14,67 @@ class Tutorial extends React.Component<{}, TutorialState> {
     super(props)
 
     this.state = {
-      open: localStorage.getItem("tutorial") != "seen",
+      open: localStorage.getItem('tutorial') != 'seen'
     }
 
     document.onkeyup = this.onKeyUp
   }
 
+  openTutorial = () => {
+    this.setState({ open: true })
+  }
+
   render() {
     if (!this.state.open) {
-      return null
+      return (
+        <button
+          className="EditorButton TutorialButton"
+          onClick={this.openTutorial}
+        >
+          ?
+        </button>
+      )
     }
 
+    return (
+      <>
+        <button
+          className="EditorButton TutorialButton"
+          onClick={this.openTutorial}
+        >
+          ?
+        </button>
+
+        {this.state.open ? this.renderTutorial() : null}
+      </>
+    )
+  }
+
+  renderTutorial() {
     return (
       <div className="TutorialBackdrop">
         <div className="Tutorial">
           <div>
-            <button className="delete" onClick={this.handleClose}>X</button>
+            <button className="delete" onClick={this.handleClose}>
+              X
+            </button>
           </div>
 
-          <TutorialPage onClose={this.handleClose}/>
+          <TutorialPage onClose={this.handleClose} />
         </div>
       </div>
     )
   }
 
   private onKeyUp = (event: KeyboardEvent) => {
-    if (this.state.open && event.key == "Escape") {
+    if (this.state.open && event.key == 'Escape') {
       this.handleClose()
     }
   }
 
   handleClose = () => {
-    localStorage.setItem("tutorial", "seen")
-    this.setState({open: false})
+    localStorage.setItem('tutorial', 'seen')
+    this.setState({ open: false })
   }
 }
 
