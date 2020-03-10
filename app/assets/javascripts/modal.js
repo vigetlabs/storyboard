bf51e950919df84ac62da7c6853ac618b197356f
DanielSBrown <danny.brown@viget.com>
2020-03-10 16:39:21 -0400

    Add in removal/adding back of buttons/modals when saving when appropriate

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,14 +1,33 @@
 var modal = document.getElementById('saveModal')
 var btn = document.getElementById('saveBtn')
 var span = document.getElementsByClassName('close')[0]
+var modalStyle = 'none'
 btn.onclick = function() {
-  modal.style.display = 'block'
+  modalStyle = 'block'
+  modal.style.display = modalStyle
 }
 span.onclick = function() {
-  modal.style.display = 'none'
+  modalStyle = 'none'
+  modal.style.display = modalStyle
 }
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = 'none'
+    modalStyle = 'none'
+    modal.style.display = modalStyle
   }
 }
+document.onkeydown = function(event) {
+  if (event.ctrlKey || event.metaKey) {
+    if (String.fromCharCode(event.which).toLowerCase() == 's') {
+      modal.style.display = 'none'
+      btn.style.display = 'none'
+    }
+  }
+}
+
+function a() {
+  modal.style.display = modalStyle
+  btn.style.display = 'block'
+}
+
+window.addEventListener('focus', a)
