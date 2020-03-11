4dcd0850aa2c239385e975f829ee7d2b59ffa561
DanielSBrown <danny.brown@viget.com>
2020-03-11 14:36:56 -0400

    Fix how I am handling the button/modal appearing. Specifically, check if the current protocol stars with http. If it doesnt (i.e. starts with file::) then hide those two elements

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,33 +1,21 @@
 var modal = document.getElementById('saveModal')
 var btn = document.getElementById('saveBtn')
 var span = document.getElementsByClassName('close')[0]
-var modalStyle = 'none'
 btn.onclick = function() {
-  modalStyle = 'block'
-  modal.style.display = modalStyle
+  modal.style.display = 'block'
 }
 span.onclick = function() {
-  modalStyle = 'none'
-  modal.style.display = modalStyle
+  modal.style.display = 'none'
 }
 window.onclick = function(event) {
   if (event.target == modal) {
-    modalStyle = 'none'
-    modal.style.display = modalStyle
+    modal.style.display = 'none'
   }
 }
-document.onkeydown = function(event) {
-  if (event.ctrlKey || event.metaKey) {
-    if (String.fromCharCode(event.which).toLowerCase() == 's') {
-      modal.style.display = 'none'
-      btn.style.display = 'none'
-    }
+window.addEventListener('load', event => {
+  if (!window.location.protocol.startsWith('http')) {
+    // Offline version
+    modal.style.display = 'none'
+    btn.style.display = 'none'
   }
-}
-
-function resetStyles() {
-  modal.style.display = modalStyle
-  btn.style.display = 'block'
-}
-
-window.addEventListener('focus', resetStyles)
+})
