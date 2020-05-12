e55a7f8eeee5f5cc1f7ff31b728ae9e91e867704
DanielSBrown <danny.brown@viget.com>
2020-05-12 15:16:52 -0400

    Testing new solution

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,20 +11,20 @@ require 'support/factory_bot.rb'
 
 include Warden::Test::Helpers
 
-# Capybara.register_driver :chrome do |app|
-#   Capybara::Selenium::Driver.new(app, browser: :chrome)
-# end
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
 
-# Capybara.register_driver :headless_chrome do |app|
-#   caps = Selenium::WebDriver::Remote::Capabilities.chrome(loggingPrefs: { browser: 'ALL' })
-#   opts = Selenium::WebDriver::Chrome::Options.new
+Capybara.register_driver :headless_chrome do |app|
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome(loggingPrefs: { browser: 'ALL' })
+  opts = Selenium::WebDriver::Chrome::Options.new
 
-#   chrome_args = %w[--headless --no-sandbox --disable-gpu --window-size=1920,1080 --remote-debugging-port=9222]
-#   chrome_args.each { |arg| opts.add_argument(arg) }
-#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: opts, desired_capabilities: caps)
-# end
+  chrome_args = %w[--headless --no-sandbox --disable-gpu --window-size=1920,1080 --remote-debugging-port=9222]
+  chrome_args.each { |arg| opts.add_argument(arg) }
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: opts, desired_capabilities: caps)
+end
 
-# Capybara.javascript_driver = :headless_chrome
+Capybara.javascript_driver = :headless_chrome
 
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
