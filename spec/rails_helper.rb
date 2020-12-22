6c3595a8af913833f3952d36b2fbaa94870fe925
David Eisinger <david.eisinger@gmail.com>
2020-12-22 17:16:30 -0500

    Get JS specs running in Docker
    Adapted from:
    
    * https://gist.github.com/varyonic/dea40abcf3dd891d204ef235c6e8dd79
    * https://lamphanqg.github.io/2019/01/20/dockerized-capybara.html

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,7 +13,18 @@ require 'support/factory_bot.rb'
 
 include Warden::Test::Helpers
 
-Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.register_driver :chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :chrome_headless
 
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
