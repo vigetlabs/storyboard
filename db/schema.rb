8fce07769e001cbd70e04c97fde79ffe86febd19
Noah Over <noahko96@gmail.com>
2022-05-19 09:51:17 -0400

    [#172] Add Story URL field to feedback form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_135224) do
+ActiveRecord::Schema.define(version: 2022_05_19_134603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_135224) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
   end
 
   create_table "thumbnails", force: :cascade do |t|
